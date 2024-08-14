56d0504fabfc6c88201f0ceac76feff144f55250
Ch0coflan <153124045+Ch0coflan@users.noreply.github.com>
2024-08-14 15:24:45 -0500

    puerta y script, movimiento de props en birthday

--- Assets/Script/collectibleItems.cs
+++ Assets/Script/collectibleItems.cs
@@ -3,6 +3,15 @@ using UnityEngine;
 public class collectibleItems : MonoBehaviour
 {
     [SerializeField] private int _objectsCollected = 0;
+    public int objectsCollected
+    {
+        get
+        { return _objectsCollected; }
+    }
+        
+
+   
+    
   
     private void OnTriggerEnter(Collider other)
     {
--- Assets/Script/pointsValidation.cs
+++ Assets/Script/pointsValidation.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class pointsValidation : MonoBehaviour
 {
-    
+    public collectibleItems items;
+    public GameObject elementoFinal;
+
+    private void Start()
+    {
+        items = FindObjectOfType<collectibleItems>();
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(items != null && items.objectsCollected >= 120)
+        {
+            elementoFinal.SetActive(true);
+        }
+    }
 }
